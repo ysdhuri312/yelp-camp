5fa93f91ce2076aced1858e4bb47d3448cb0b4a4
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-16 00:31:31 +0530

    add dummy data

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+
+    await camp.save();
+  }
+};
+
+// seedDB().then(() => {
+//   mongoose.connection.close();
+// });

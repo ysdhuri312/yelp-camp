871e51aad0456906e87f9ddd35cb1f4cb5a9f0c4
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-17 22:21:21 +0530

    Add new campground form with backend controller

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'node:path';
-import connectDB from './db/db.js';
+import connectDB from './config/db.js';
 import Campground from './models/campground.model.js';
 
 
@@ -11,8 +11,11 @@ app.set("view engine", 'ejs');
 app.set('views', path.join(path.resolve(), 'views'));
 connectDB();
 
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
+// Routes
 app.get('/', (req, res) => {
     console.log('');
     res.render('home');
@@ -23,11 +26,20 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campground/index', { campgrounds });
 })
 
-app.get('/campground/:id', async (req, res) => {
+app.get('/campground/new', (req, res) => {
+    res.render('campground/new');
+})
 
+app.post('/campground', async (req, res) => {
+    const campground = await Campground.create({ ...req.body })
+    res.redirect(`/campground/${campground._id} `);
+})
+
+app.get('/campground/:id', async (req, res) => {
     const campground = await Campground.findById({ _id: req.params.id });
     res.render('campground/show', { campground });
 })
+
 
 app.listen(PORT, (req, res) => {
     console.log("✅ Server running on http://localhost:3000");

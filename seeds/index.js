08bd565a7f421b082d65b4587537da35c7c0f82c
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-18 01:51:38 +0530

    Add some data usin seeds like image-link, description

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,9 @@ const seedDB = async () => {
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/id/${random1000}/2500/1667`,
+      price: `${random1000}`,
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
     });
 
     await camp.save();

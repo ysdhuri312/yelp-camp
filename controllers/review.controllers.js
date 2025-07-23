ba954638f94b2a306739dddda3c1bc580ed78fd6
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-24 01:17:56 +0530

    change file names

---
+++ controllers/review.controllers.js
@@ -0,0 +1,31 @@
+/** @format */
+
+import catchAsyncError from '../utils/catchAsyncError.js';
+import Review from '../models/review.model.js';
+import Campground from '../models/campground.model.js';
+import customError from '../utils/CustomError.js';
+
+const createReview = catchAsyncError(async (req, res, next) => {
+  const { rating, body } = req.body.review;
+
+  if (!rating || !body) {
+    throw new customError(400, 'All fields are required.', 'CreateReviewRoute');
+  }
+
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  req.flash('success', 'Review created successfully');
+  res.redirect(`/campground/${campground._id}`);
+});
+
+const deleteReview = catchAsyncError(async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  req.flash('success', 'Review deleted successfully');
+  res.redirect(`/campground/${id}`);
+});
+export { createReview, deleteReview };

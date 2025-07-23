c5047268c4b7511b800f0e2d2b5426e2d2ce4039
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-23 22:54:32 +0530

    configure session and flash

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 import express from 'express';
 import methodOverride from 'method-override';
 import engine from 'ejs-mate';
+import session from 'express-session';
+import flash from 'connect-flash';
 
 import path from 'node:path';
 
@@ -26,7 +28,27 @@ app.use(express.static(path.join(path.resolve(), 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+var sessionConfig = {
+  secret: 'qwerty',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
 
+// Middleware for flash messages
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Home Route
 app.get('/', (req, res, next) => {
   res.render('home');
 });
@@ -35,8 +57,11 @@ app.get('/', (req, res, next) => {
 app.use('/campground', campgroundRoutes);
 app.use('/campground', reviewRoutes);
 
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+// 404 error route
 app.use((req, res, next) => {
-  // console.log(`${req.method} ${req.path}`)
+  console.log(`${req.method} ${req.path}`);
   next(new CustomError(404, 'Page not found...', undefined));
 });
 

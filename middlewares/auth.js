7b57dac3e282d54a86a0c9ac7233d937daa358f0
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-25 23:46:14 +0530

    Add middleware for user are signin or singout

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,39 @@
 /** @format */
 
 import jwt from 'jsonwebtoken';
+import CustomError from '../utils/CustomError.js';
+import catchAsyncError from '../utils/catchAsyncError.js';
 
-const isLoggedIn = (req, res, next) => {};
+export const generateToken = (payload, secret) => {
+  return jwt.sign(payload, secret, { expiresIn: '7d' });
+};
+
+export const isLoggedIn = catchAsyncError(async (req, res, next) => {
+  const token = req.session.userId;
+
+  if (!token) {
+    res.locals.isAuthenticated = false;
+    req.flash('error', 'Must be signed in first');
+    res.redirect('/userl/signin');
+    return new CustomError(403, 'Must be signed in first');
+  }
+
+  if (token) {
+    var decoded = await jwt.verify(token, 'thisissecret');
+    const verifyToken = jwt.verify(
+      token,
+      'thisissecret',
+      function (err, decoded) {
+        return true;
+      }
+    );
+
+    if (verifyToken) {
+      res.locals.isAuthenticated = true;
+      return next();
+    } else {
+      res.locals.isAuthenticated = true;
+      return new CustomError(403, 'Must be signed in first');
+    }
+  }
+});

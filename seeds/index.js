7042033ff9bc0d626e0533681c93e4baf27e72af
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-07-27 00:49:43 +0530

    Add author in all campgrounds  dummy data

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,16 @@
+/** @format */
+
 import mongoose from 'mongoose';
 import Campground from '../models/campground.model.js';
 import cities from './cities.js';
 import { places, descriptors } from './seedHelpers.js';
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp");
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection Error:"));
-db.once("open", () => {
-  console.log("Database Connected");
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', () => {
+  console.log('Database Connected');
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
@@ -18,11 +20,13 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
+      author: '6885239117bd7a1176a42831',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: `https://picsum.photos/id/${random1000}/2500/1667`,
       price: `${random1000}`,
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     });
 
     await camp.save();

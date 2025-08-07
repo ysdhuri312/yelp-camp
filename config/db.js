b14684f0d8f8ea064d68b0155b29ce87d3deb7be
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-08-07 22:04:02 +0530

    Connect mongodb atlas

--- config/db.js
+++ config/db.js
@@ -1,17 +1,21 @@
+/** @format */
+
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log(
+      `✅ Database conneceted on ${mongoose.connection.name} at ${mongoose.connection.host}:${mongoose.connection.port}`
+    );
 
-    try {
-        await mongoose.connect("mongodb://localhost:27017/yelp-camp");
-        console.log(`✅ Database conneceted on ${mongoose.connection.name} at ${mongoose.connection.host}:${mongoose.connection.port}`);
-
-        mongoose.connection.on('error', console.error.bind(console, "Database connection error"));
-
-    } catch (error) {
-        console.error("❌ Failed to connect to MongoDB:", error);
-    }
-
-}
+    mongoose.connection.on(
+      'error',
+      console.error.bind(console, 'Database connection error')
+    );
+  } catch (error) {
+    console.error('❌ Failed to connect to MongoDB:', error);
+  }
+};
 
 export default connectDB;
